de821370ede197e2ab8da75946bd6642de8d7253
Premsai Peddi <premsaip@rocketmail.com>
2025-01-05 12:23:42 -0500

    Partially completed socket initialization

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@
 #include <nlohmann/json.hpp>
 #include <thread>
 #include <atomic>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib") // Link Winsock library
+
+
 
 using json = nlohmann::json;
 
@@ -16,12 +21,20 @@ class Node{
         int PORT;
         json DATA;
         std::atomic<bool> running{true}; // Atomic flag for threads
+        std::string HOST = "localhost";
+        SOCKET node_socket;
 
         //Constructor
         Node(std::string nodePath, std::string port){
         NODEPATH = parsePath(nodePath);
         PORT = parsePort(port);
         DATA = parseJSON(NODEPATH);
+        initializewinsock();
+        node_socket = initializeSocket();
+        }
+        //Destructor
+        ~Node(){
+            WSACleanup();
         }
 
         // Input thread : 2 commands - kill, filename
@@ -33,7 +46,7 @@ class Node{
                 std::cout << "Enter Command (Kill/filename): " << std::endl;
                 std::cin >> command;
 
-                    if (command == std::string("kill")){
+                    if (command =="kill"){
                         std::cout << "Shutting down now..." << std::endl;
                         running = false;
                     }
@@ -46,9 +59,52 @@ class Node{
             }
         }
 
+        void client_thread(){
+            //My address
+            sockaddr_in client_addr;
+            client_addr.sin_family = AF_INET;
+            client_addr.sin_port = htons(PORT);
+            client_addr.sin_addr.s_addr = inet_addr(HOST.c_str());
+
+            if(bind(node_socket, reinterpret_cast<sockaddr*>(&client_addr), sizeof(client_addr)) == SOCKET_ERROR){
+                std::cerr << "Error binding socket" << std::endl;
+                running = false;
+            }
+            
+            
+
+            
+        }
+
 
     private:
         //Other Functions:
+
+        WSADATA wsa_data; //data about winsock implementation
+        // Initialize winsock
+        void initializewinsock(){
+            int wsa_result = WSAStartup(MAKEWORD(2,2), &wsa_data);
+            if (wsa_result != 0){
+                std::cerr << "Winsock initialization failed" << std::endl;
+                running = false;
+            }
+            else{
+                std::cout << "Winsock initialization success" <<std::endl;
+            }
+        }
+        // Initialize socket
+        SOCKET initializeSocket(){
+            SOCKET node_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+            if (node_socket == INVALID_SOCKET){
+                std::cerr << "Error creating UDP Socket" << std::endl;
+                running = false;
+            }
+            else{
+                std::cout << "Creating socket successful" << std::endl;
+            }
+            return node_socket;
+
+        }
 
         //Parse Path
         std::string parsePath(std::string path){
